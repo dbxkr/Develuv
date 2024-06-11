047e5eea4a2c9471e9bbe5423183d4833c085d2f
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2024-06-11 19:11:33 +0900

    signup 4,5 back and front by hhy

---
+++ Develuv/Back/src/main/java/kr/bit/dto/NbtiDto.java
@@ -0,0 +1,13 @@
+package kr.bit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class NbtiDto {
+
+    private String user_id;
+    private String user_nbti;
+    private String[] nbti;
+
+}
