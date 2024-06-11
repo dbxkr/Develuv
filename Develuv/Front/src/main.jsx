da0798ff179c1ab9a6408175b787bea7032bae85
Marckel9804 <ckdgh10421@gmail.com>
2024-06-11 19:05:55 +0900

    regi3 is update

--- Develuv/Front/src/main.jsx
+++ Develuv/Front/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Regi5Form from "./components/register/Register5/Regi5Form.jsx";
+import Regi3Form from "./components/register/Register3/Regi3Form.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<App />*/}
-    {/*<Regi3Form />*/}
-    <Regi5Form/>
+    <Regi3Form />
+    {/*<Regi5Form/>*/}
   </React.StrictMode>
 )
