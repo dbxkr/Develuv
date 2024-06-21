d41d2cfff9528430727c3bee223bece23d6728d3
Marckel9804 <ckdgh10421@gmail.com>
2024-06-21 14:34:41 +0900

    before merge jyt

--- Develuv/Back/src/main/java/kr/bit/service/MatchingService.java
+++ Develuv/Back/src/main/java/kr/bit/service/MatchingService.java
@@ -79,8 +79,6 @@ public class MatchingService {
         double c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
         double d = r * c;
 
-        System.out.println(d);
-
         return d;
 
     }
