980c9b1ebf08f5cdaac5ad000a4614f9a8217d35
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2024-06-27 17:46:31 +0900

    퀴즈 가져오기 기능  수정! Front : UserQuiz, QuizForm, QuizContent / BACK : QuizFormController, UserQuizzMapper 수정 , Back : QuizzOppoDTO 추가

--- Develuv/Back/src/main/java/kr/bit/controller/QuizFormController.java
+++ Develuv/Back/src/main/java/kr/bit/controller/QuizFormController.java
@@ -1,11 +1,10 @@
 package kr.bit.controller;
 
 import kr.bit.dto.QuizFormDTO;
+import kr.bit.dto.QuizOppoDTO;
 import kr.bit.mapper.UserQuizMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,22 @@ public class QuizFormController {
     public List<QuizFormDTO> getChoices(@PathVariable("qId") int q_id) {
         List<QuizFormDTO> choices = userQuizMapper.getChoies(q_id);
         return choices;
+    }
+
+    @PostMapping("/quiz/user")
+    public void updateQuizCount(@RequestBody QuizOppoDTO quizOppoDTO) {
+        System.out.println(quizOppoDTO);
+        userQuizMapper.updateQuizCount(quizOppoDTO);
+    }
+
+    @GetMapping("/quiz/check")
+    public int getQuizCount(@RequestParam("user_id") String user_id,
+                            @RequestParam("oppo_id") String oppo_id) {
+        QuizOppoDTO quizOppoDTO = new QuizOppoDTO();
+        quizOppoDTO.setUser_id(user_id);
+        quizOppoDTO.setOppo_id(oppo_id);
+        int result = userQuizMapper.getQuizCount(quizOppoDTO);
+        return result;
     }
 
 }
--- Develuv/Back/src/main/java/kr/bit/mapper/UserQuizMapper.java
+++ Develuv/Back/src/main/java/kr/bit/mapper/UserQuizMapper.java
@@ -1,7 +1,9 @@
 package kr.bit.mapper;
 
 import kr.bit.dto.QuizFormDTO;
+import kr.bit.dto.QuizOppoDTO;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -12,4 +14,10 @@ public interface UserQuizMapper {
 
     @Select("select * from userquiz where user_id = #{user_id}")
     public List<QuizFormDTO> getQuizes(String user_id);
+
+    @Update("update opponent set quiz = 1 where user_id = #{user_id} and oppo_id = #{oppo_id}")
+    public void updateQuizCount(QuizOppoDTO quizOppoDTO);
+
+    @Select("select quiz from opponent where user_id = #{user_id} and oppo_id = #{oppo_id}")
+    public int getQuizCount(QuizOppoDTO quizOppoDTO);
 }
