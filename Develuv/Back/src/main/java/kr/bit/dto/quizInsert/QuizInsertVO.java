60ebf163329af130452d3a683ab7d6be6449b9cf
Marckel9804 <ckdgh10421@gmail.com>
2024-07-03 01:24:45 +0900

    fix myquiz to update

--- Develuv/Back/src/main/java/kr/bit/dto/quizInsert/QuizInsertVO.java
+++ Develuv/Back/src/main/java/kr/bit/dto/quizInsert/QuizInsertVO.java
@@ -1,13 +1,15 @@
 package kr.bit.dto.quizInsert;
 
 import lombok.Data;
+
 import java.util.List;
 
 @Data
-public class QuizInsertDTO {
+public class QuizInsertVO {
+    private int q_id;  // 이 필드를 추가합니다.
     private String user_id;
     private int q_num;
     private String quiz;
     private int answer;
-    private List<ChoiceInsertDTO> choices;
+    private List<ChoiceInsertVO> choices;
 }
