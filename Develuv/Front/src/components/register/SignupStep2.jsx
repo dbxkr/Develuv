281900652a706b0738b0a3ef2a8e1404485079e2
JEONYUTAK <btt0408@gmail.com>
2024-07-03 20:22:38 +0900

    b m

--- Develuv/Front/src/components/register/SignupStep2.jsx
+++ Develuv/Front/src/components/register/SignupStep2.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SignupStep2 = ({
   setProgress,
@@ -14,19 +14,19 @@ const SignupStep2 = ({
   userIdAvailable,
   setUserIdAvailable,
 }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [showVerificationField, setShowVerificationField] = useState(false)
-  const [verificationMessage, setVerificationMessage] = useState('')
-  const [userIdCheckMessage, setUserIdCheckMessage] = useState('')
+  const [showVerificationField, setShowVerificationField] = useState(false);
+  const [verificationMessage, setVerificationMessage] = useState("");
+  const [userIdCheckMessage, setUserIdCheckMessage] = useState("");
 
   useEffect(() => {
-    if (fieldTouched.user_id) validateUserId(formData.user_id)
-    if (fieldTouched.user_pw) validatePassword(formData.user_pw)
+    if (fieldTouched.user_id) validateUserId(formData.user_id);
+    if (fieldTouched.user_pw) validatePassword(formData.user_pw);
     if (fieldTouched.user_pw_confirm)
-      validatePasswordConfirm(formData.user_pw, formData.user_pw_confirm)
-    if (fieldTouched.user_email) validateEmail(formData.user_email)
-  }, [formData, fieldTouched])
+      validatePasswordConfirm(formData.user_pw, formData.user_pw_confirm);
+    if (fieldTouched.user_email) validateEmail(formData.user_email);
+  }, [formData, fieldTouched]);
 
   if (state && state.user != null) {
     formData.provider = "develuv";
@@ -50,89 +50,89 @@ const SignupStep2 = ({
     }
   }
   useEffect(() => {
-    console.log(state)
-  }, [])
+    console.log(state);
+  }, []);
 
   const handleChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-    if (name === 'user_id') {
-      setUserIdCheckMessage('') // 아이디 필드가 수정되면 중복 확인 메시지를 숨김
-      setUserIdAvailable(false)
+    });
+    if (name === "user_id") {
+      setUserIdCheckMessage(""); // 아이디 필드가 수정되면 중복 확인 메시지를 숨김
+      setUserIdAvailable(false);
     }
-  }
+  };
 
   const handleFocus = (e) => {
-    const { name } = e.target
+    const { name } = e.target;
     setFieldTouched({
       ...fieldTouched,
       [name]: true,
-    })
-  }
+    });
+  };
 
   const handleBlur = (e) => {
-    const { name } = e.target
+    const { name } = e.target;
     setFieldTouched({
       ...fieldTouched,
       [name]: false,
-    })
-  }
+    });
+  };
 
   const validateUserId = (user_id) => {
-    let error = ''
+    let error = "";
     if (user_id.length < 6) {
-      error = '아이디는 6글자 이상이어야 합니다.'
+      error = "아이디는 6글자 이상이어야 합니다.";
     }
-    setFormErrors((prevErrors) => ({ ...prevErrors, user_id: error }))
-  }
+    setFormErrors((prevErrors) => ({ ...prevErrors, user_id: error }));
+  };
 
   const validatePassword = (user_pw) => {
-    let error = ''
+    let error = "";
     if (user_pw.length < 8) {
-      error = '비밀번호는 8글자 이상이어야 합니다.'
+      error = "비밀번호는 8글자 이상이어야 합니다.";
     } else if (!/\d/.test(user_pw)) {
-      error = '비밀번호에는 숫자가 포함되어야 합니다.'
+      error = "비밀번호에는 숫자가 포함되어야 합니다.";
     }
-    setFormErrors((prevErrors) => ({ ...prevErrors, user_pw: error }))
-  }
+    setFormErrors((prevErrors) => ({ ...prevErrors, user_pw: error }));
+  };
 
   const validatePasswordConfirm = (user_pw, user_pw_confirm) => {
-    let error = ''
+    let error = "";
     if (user_pw !== user_pw_confirm) {
-      error = '비밀번호가 일치하지 않습니다.'
+      error = "비밀번호가 일치하지 않습니다.";
     }
-    setFormErrors((prevErrors) => ({ ...prevErrors, user_pw_confirm: error }))
-  }
+    setFormErrors((prevErrors) => ({ ...prevErrors, user_pw_confirm: error }));
+  };
 
   const validateEmail = (user_email) => {
-    let error = ''
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    let error = "";
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(user_email)) {
-      error = '유효한 이메일 주소를 입력하세요.'
+      error = "유효한 이메일 주소를 입력하세요.";
     }
-    setFormErrors((prevErrors) => ({ ...prevErrors, user_email: error }))
-  }
+    setFormErrors((prevErrors) => ({ ...prevErrors, user_email: error }));
+  };
 
   const handleCheckId = async () => {
     if (!formErrors.user_id) {
       try {
         const response = await axios.get(
           `http://localhost:8080/user/checkUserId?userId=${formData.user_id}`
-        )
-        setUserIdAvailable(response.data)
+        );
+        setUserIdAvailable(response.data);
         setUserIdCheckMessage(
           response.data
-            ? '사용 가능한 아이디입니다.'
-            : '이미 사용 중인 아이디입니다.'
-        )
+            ? "사용 가능한 아이디입니다."
+            : "이미 사용 중인 아이디입니다."
+        );
       } catch (error) {
-        console.error('Error checking user ID:', error)
+        console.error("Error checking user ID:", error);
       }
     }
-  }
+  };
 
   const handleSendVerificationCode = async () => {
     if (!formErrors.user_email) {
@@ -142,78 +142,78 @@ const SignupStep2 = ({
             `http://localhost:8080/user/sendVerificationCode?email=${formData.user_email}`
           )
           .then((res) => {
-            console.log(res)
+            console.log(res);
             if (
-              res.data.msg === '이미 가입된 이메일입니다.' ||
+              res.data.msg === "이미 가입된 이메일입니다." ||
               res.data.msg ===
-                '메일 전송에 실패했습니다. 주소를 다시 확인해주세요.'
+                "메일 전송에 실패했습니다. 주소를 다시 확인해주세요."
             ) {
-              setShowVerificationField(false) // 이미 가입된 이메일일 경우 필드 숨김
+              setShowVerificationField(false); // 이미 가입된 이메일일 경우 필드 숨김
             } else {
-              setShowVerificationField(true) // 인증 번호 입력 필드 표시
+              setShowVerificationField(true); // 인증 번호 입력 필드 표시
             }
-            alert(res.data.msg) // 서버에서 보낸 메시지 알림
-          })
+            alert(res.data.msg); // 서버에서 보낸 메시지 알림
+          });
 
         // console.log(response.data) // 응답 데이터를 로그로 출력
       } catch (error) {
-        console.error('Error sending verification code:', error)
+        console.error("Error sending verification code:", error);
       }
     }
-  }
+  };
 
   const handleVerifyCode = async () => {
     try {
       const response = await axios.post(
         `http://localhost:8080/user/verifyCode?email=${formData.user_email}&code=${formData.verification_code}`
-      )
+      );
       if (response.data.isValid) {
-        setVerificationMessage('인증이 완료되었습니다.')
+        setVerificationMessage("인증이 완료되었습니다.");
       } else {
-        setVerificationMessage('인증에 실패하였습니다.')
+        setVerificationMessage("인증에 실패하였습니다.");
       }
     } catch (error) {
-      setVerificationMessage('Error verifying code: ' + error.message)
+      setVerificationMessage("Error verifying code: " + error.message);
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     // 필수 필드들이 비어 있는지 확인
     const requiredFields = [
-      'user_id',
-      'user_pw',
-      'user_pw_confirm',
-      'user_email',
-      // "verification_code",
-      'user_name',
-      'user_birth',
-      'user_phone',
-    ]
+      "user_id",
+      "user_pw",
+      "user_pw_confirm",
+      "user_email",
+      "verification_code",
+      "user_name",
+      "user_birth",
+      "user_phone",
+    ];
 
-    let allFieldsFilled = true
-    const newFormErrors = { ...formErrors }
+    let allFieldsFilled = true;
+    const newFormErrors = { ...formErrors };
 
     requiredFields.forEach((field) => {
       if (!formData[field]) {
-        newFormErrors[field] = '이 필드는 필수입니다.'
-        allFieldsFilled = false
+        newFormErrors[field] = "이 필드는 필수입니다.";
+        allFieldsFilled = false;
       } else {
-        newFormErrors[field] = ''
+        newFormErrors[field] = "";
       }
-    })
+    });
 
-    setFormErrors(newFormErrors)
+    setFormErrors(newFormErrors);
 
     if (!allFieldsFilled) {
-      alert('모든 필수 필드를 입력하세요.')
-      return
+      alert("모든 필수 필드를 입력하세요.");
+      return;
     }
 
     if (!userIdAvailable) {
-      alert('아이디 중복 여부를 확인하세요.')
-      return
+      alert("아이디 중복 여부를 확인하세요.");
+      return;
     }
 
     // 기존 유효성 검사를 통과했는지 확인
@@ -223,11 +223,11 @@ const SignupStep2 = ({
       formErrors.user_pw_confirm ||
       formErrors.user_email
     ) {
-      alert('입력한 필드에 오류가 있습니다. 다시 확인해주세요.')
-      return
+      alert("입력한 필드에 오류가 있습니다. 다시 확인해주세요.");
+      return;
     }
 
-    setProgress(3)
+    setProgress(3);
     // try {
     // const response = await axios.post(
     //   "http://localhost:8080/user/signup",
@@ -238,7 +238,7 @@ const SignupStep2 = ({
     //   console.error("Error signing up:", error);
     //   alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
     // }
-  }
+  };
 
   return (
     <div className="quiz-container">
@@ -259,7 +259,7 @@ const SignupStep2 = ({
             className="with-button-input"
             readOnly={state && !!state.user}
             style={
-              state && state.user ? { backgroundColor: 'lightgray' } : null
+              state && state.user ? { backgroundColor: "lightgray" } : null
             }
           />
           <button
@@ -287,7 +287,7 @@ const SignupStep2 = ({
             onFocus={state && state.user ? null : handleFocus}
             onBlur={state && state.user ? null : handleBlur}
             style={
-              state && state.user ? { backgroundColor: 'lightgray' } : null
+              state && state.user ? { backgroundColor: "lightgray" } : null
             }
             readOnly={state && !!state.user}
           />
@@ -305,7 +305,7 @@ const SignupStep2 = ({
             onFocus={state && state.user ? null : handleFocus}
             onBlur={state && state.user ? null : handleBlur}
             style={
-              state && state.user ? { backgroundColor: 'lightgray' } : null
+              state && state.user ? { backgroundColor: "lightgray" } : null
             }
             readOnly={state && !!state.user}
           />
@@ -325,14 +325,14 @@ const SignupStep2 = ({
             className="with-button-input"
             readOnly={
               state &&
-              (state.provider === 'naver' || state.provider === 'google')
+              (state.provider === "naver" || state.provider === "google")
                 ? true
                 : false
             }
             style={
               state &&
-              (state.provider === 'naver' || state.provider === 'google')
-                ? { backgroundColor: 'lightgray' }
+              (state.provider === "naver" || state.provider === "google")
+                ? { backgroundColor: "lightgray" }
                 : null
             }
           />
@@ -341,7 +341,7 @@ const SignupStep2 = ({
             className="check-button"
             onClick={
               state &&
-              (state.provider === 'naver' || state.provider === 'google')
+              (state.provider === "naver" || state.provider === "google")
                 ? null
                 : handleSendVerificationCode
             }
@@ -375,7 +375,7 @@ const SignupStep2 = ({
           {verificationMessage && (
             <p
               className={`verification-message ${
-                verificationMessage.includes('완료') ? 'success' : 'error'
+                verificationMessage.includes("완료") ? "success" : "error"
               }`}
             >
               {verificationMessage}
@@ -391,14 +391,14 @@ const SignupStep2 = ({
             value={formData.user_name}
             readOnly={
               state &&
-              (state.provider === 'naver' || state.provider === 'google')
+              (state.provider === "naver" || state.provider === "google")
                 ? true
                 : false
             }
             style={
               state &&
-              (state.provider === 'naver' || state.provider === 'google')
-                ? { backgroundColor: 'lightgray' }
+              (state.provider === "naver" || state.provider === "google")
+                ? { backgroundColor: "lightgray" }
                 : null
             }
             onChange={handleChange}
@@ -412,10 +412,10 @@ const SignupStep2 = ({
             type="date"
             name="user_birth"
             value={formData.user_birth}
-            readOnly={state && state.provider === 'naver' ? true : false}
+            readOnly={state && state.provider === "naver" ? true : false}
             style={
-              state && state.provider === 'naver'
-                ? { backgroundColor: 'lightgray' }
+              state && state.provider === "naver"
+                ? { backgroundColor: "lightgray" }
                 : null
             }
             onChange={handleChange}
@@ -430,10 +430,10 @@ const SignupStep2 = ({
             name="user_phone"
             placeholder="휴대전화번호"
             value={formData.user_phone}
-            readOnly={state && state.provider === 'naver' ? true : false}
+            readOnly={state && state.provider === "naver" ? true : false}
             style={
-              state && state.provider === 'naver'
-                ? { backgroundColor: 'lightgray' }
+              state && state.provider === "naver"
+                ? { backgroundColor: "lightgray" }
                 : null
             }
             onChange={handleChange}
@@ -447,7 +447,7 @@ const SignupStep2 = ({
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignupStep2
+export default SignupStep2;
